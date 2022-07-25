dc04120f8520d31f6deaa362691180ca270c4ded
Пётр Бурдуев <burduev.p@gmail.com>
2022-07-25 10:21:14 +0300

    first

--- src/js/basic.js
+++ src/js/basic.js
@@ -1,31 +1,27 @@
-export default function Character(name, type) {
-  this.name = name;
-  this.type = type;
-  this.health = 100;
-  this.level = 1;
-  this.attack = undefined;
-  this.deffence = undefined;
+export default class Character {
+  constructor(name, type) {
+    const types = [
+      "Bowerman",
+      "Daemon",
+      "Magician",
+      "Swordsman",
+      "Undead",
+      "Zombie",
+    ];
 
-  const typeCharacter = [
-    "Bowerman",
-    "Daemon",
-    "Magician",
-    "Swordsman",
-    "Undead",
-    "Zombie",
-  ];
+    if (name.length < 2 || name.length > 10 || typeof name !== "string") {
+      throw new Error("Ошибка в name");
+    } else {
+      this.name = name;
+    }
 
-  if (typeof name !== "string") {
-    throw new Error("Ошибка! Имя должно быть строкой!");
-  }
+    if (!types.includes(type)) {
+      throw new Error("Ошибка в type");
+    } else {
+      this.type = type;
+    }
 
-  if (name.length < 2 || name.length > 10) {
-    throw new Error(
-      "Ошибка: Параметр name должен содержать от 2 до 10 символов!"
-    );
-  }
-
-  if (!typeCharacter.includes(type)) {
-    throw new Error("Ошибка! Такой персонаж не существует!");
+    this.health = 100;
+    this.level = 1;
   }
 }
